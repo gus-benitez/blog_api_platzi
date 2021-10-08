4bf9b1e6bce9fe658d9a5fed307a80655d969845
gus-benitez <ing.benitez.gustavo@gmail.com>
2021-10-08 14:03:38 -0300

    Implement the tests for Post and Update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,11 @@
 class PostsController < ApplicationController
+  rescue_from Exception do |error|
+    render json: {error: error.message}, status: :internal_error  
+  end
+  rescue_from ActiveRecord::RecordInvalid do |error|
+    render json: {error: error.message}, status: :unprocessable_entity  
+  end
+
   # GET /posts
   def index
     posts = Post.where(published: true)
@@ -8,5 +15,24 @@ class PostsController < ApplicationController
   def show
     post = Post.find(params[:id])
     render json: post, status: :ok
+  end
+  # POST /posts
+  def create
+    post = Post.create!(create_params)
+    render json: post, status: :created
+  end
+  # PUT /posts/{id}
+  def update
+    post = Post.find(params[:id])
+    post.update!(update_params)
+    render json: post, status: :ok
+  end
+
+  private
+  def create_params
+    params.require(:post).permit(:title, :content, :published, :user_id)
+  end
+  def update_params
+    params.require(:post).permit(:title, :content, :published)
   end
 end
