ca45b65aee734cc1ac4799d27b063fdcd24412ab
gus-benitez <ing.benitez.gustavo@gmail.com>
2021-10-09 21:49:19 -0300

    Implement functionality to filter posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,9 @@ class PostsController < ApplicationController
   # GET /posts
   def index
     posts = Post.where(published: true)
+    if !params[:search].nil? && params[:search].present?
+      posts = PostsSearchService.search(posts, params[:search])
+    end
     render json: posts, status: :ok
   end
   # GET /posts/{id}
