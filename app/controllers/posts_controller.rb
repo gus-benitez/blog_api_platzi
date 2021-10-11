6caec0713dd1d7d7a0a97c0efac7e806a4492fef
gus-benitez <ing.benitez.gustavo@gmail.com>
2021-10-11 12:15:19 -0300

    Add authentication to Post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+  # "!" added by convention, when an action can modify the behavior of the request
+  before_action :authenticate_user!, only: %i[create update]
+
   rescue_from Exception do |error|
     render json: { error: error.message }, status: :internal_error
   end
@@ -40,5 +43,20 @@ class PostsController < ApplicationController
 
   def update_params
     params.require(:post).permit(:title, :content, :published)
+  end
+
+  def authenticate_user!
+    # Bearer xxxxxx
+    token_regex = /Bearer (\w+)/
+    # Leer HEADER de auth
+    headers = request.headers
+    # Verificar que sea valido
+    if headers['Authorization'].present? && headers['Authorization'].match(token_regex)
+      token = headers['Authorization'].match(token_regex)[1]
+      # Verificar que el token corresponda a un user
+      return if (Current.user = User.find_by_auth_token(token))
+    end
+
+    render json: { error: 'Unauthorized' }, status: :unauthorized
   end
 end
