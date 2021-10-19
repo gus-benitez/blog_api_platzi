2a4edd3dad72f82b6ecb6d4cce7e0c65a6e9b661
gus-benitez <ing.benitez.gustavo@gmail.com>
2021-10-18 22:16:46 -0300

    Caching

--- app/services/posts_search_service.rb
+++ app/services/posts_search_service.rb
@@ -1,5 +1,13 @@
 class PostsSearchService
-  def self.search(curr_posts, query)
-    curr_posts.where("title like '%#{query}%'")
+  def self.search(curr_post, query)
+    # Agregar cache para optimizar
+    # Funciona como llave valor {k => v}
+    posts_ids = Rails.cache.fetch("posts_search/#{query}", expires_in: 1.hours) do
+      # We only store the IDs in Cache
+      curr_post.where("title like '%#{query}%'").map(&:id)
+    end
+
+    # Through the IDs, we look for the publications
+    curr_post.where(id: posts_ids)
   end
 end
